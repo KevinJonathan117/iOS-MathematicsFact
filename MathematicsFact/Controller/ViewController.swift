fc5559a278fbc46b121018a9bede8b5d43eea424
KevinJonathan117 <kevinjonathan3010@gmail.com>
2021-05-30 14:39:54 +0700

    Tableview now works

--- MathematicsFact/Controller/ViewController.swift
+++ MathematicsFact/Controller/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var mathematicsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mathematicsTableView.dataSource = self
+        mathematicsTableView.register(UINib(nibName: "MathematicsTableViewCell", bundle: nil), forCellReuseIdentifier: "mathematicsCell")
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mathematicsTypes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "mathematicsCell", for: indexPath) as? MathematicsTableViewCell {
+            let mathematicType = mathematicsTypes[indexPath.row]
+            cell.titleMathematics.text = mathematicType.name
+            cell.photoMathematics.image = mathematicType.photo
+            
+            cell.photoMathematics.layer.cornerRadius = cell.photoMathematics.frame.height / 3
+            cell.photoMathematics.clipsToBounds = true
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
 }
 
