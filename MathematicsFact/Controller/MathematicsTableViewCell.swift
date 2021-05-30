97470639a8d380a77273cd78bf3a1f0f4174e70e
KevinJonathan117 <kevinjonathan3010@gmail.com>
2021-05-30 14:07:07 +0700

    Added table cell and xib files, added required files

---
+++ MathematicsFact/Controller/MathematicsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MathematicsTableViewCell.swift
+//  MathematicsFact
+//
+//  Created by Kevin Jonathan on 30/05/21.
+//
+
+import UIKit
+
+class MathematicsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoMathematics: UIImageView!
+    @IBOutlet weak var titleMathematics: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MathematicsFact/Model/MathematicsType.swift
@@ -0,0 +1,15 @@
+//
+//  MathematicsType.swift
+//  MathematicsFact
+//
+//  Created by Kevin Jonathan on 30/05/21.
+//
+
+import Foundation
+import UIKit
+ 
+struct MathematicsType {
+    let photo: UIImage
+    let name: String
+    let description: String
+}
